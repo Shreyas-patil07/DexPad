1ac272b010614068682f49524e047b3644819067
Shreyasrp07 <3shreyas2007@gmail.com>
2026-09-06 03:01:44 +0530

    Add local DexPad state bridge

--- src/preload/preload.cjs
+++ src/preload/preload.cjs
@@ -3,7 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('dexpad', {
   getState: () => ipcRenderer.invoke('dexpad:get-state'),
   setState: (state) => ipcRenderer.invoke('dexpad:set-state', state),
+  saveCards: (cards) => ipcRenderer.invoke('dexpad:save-cards', cards),
   openWorkspace: () => ipcRenderer.invoke('dexpad:open-workspace'),
   refreshWallpaper: () => ipcRenderer.invoke('dexpad:refresh-wallpaper'),
-  openUrl: (url) => ipcRenderer.invoke('dexpad:open-url', url)
+  openUrl: (url) => ipcRenderer.invoke('dexpad:open-url', url),
+  onStateUpdated: (callback) => {
+    const listener = (_event, state) => callback(state);
+    ipcRenderer.on('dexpad:state-updated', listener);
+    return () => ipcRenderer.removeListener('dexpad:state-updated', listener);
+  }
 });
