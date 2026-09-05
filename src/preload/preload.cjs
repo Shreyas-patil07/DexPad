abf1267ff2adac643af88aa669031fbd72cf9383
Shreyasrp07 <3shreyas2007@gmail.com>
2026-09-06 02:53:03 +0530

    Add wallpaper refresh IPC bridge

--- src/preload/preload.cjs
+++ src/preload/preload.cjs
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('dexpad', {
   getState: () => ipcRenderer.invoke('dexpad:get-state'),
   setState: (state) => ipcRenderer.invoke('dexpad:set-state', state),
   openWorkspace: () => ipcRenderer.invoke('dexpad:open-workspace'),
+  refreshWallpaper: () => ipcRenderer.invoke('dexpad:refresh-wallpaper'),
   openUrl: (url) => ipcRenderer.invoke('dexpad:open-url', url)
 });
