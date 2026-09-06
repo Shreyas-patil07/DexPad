0c924e0d49b896a47b8680e74108c55a8fa50646
Shreyasrp07 <3shreyas2007@gmail.com>
2026-09-06 06:17:11 +0530

    feat: expose atomic workspace persistence API

--- src/preload/preload.cjs
+++ src/preload/preload.cjs
@@ -6,10 +6,12 @@ contextBridge.exposeInMainWorld('dexpad', {
   setWallpaperMode: (enabled) => ipcRenderer.invoke('dexpad:set-wallpaper-mode', enabled),
   setState: (state) => ipcRenderer.invoke('dexpad:set-state', state),
   saveCards: (cards) => ipcRenderer.invoke('dexpad:save-cards', cards),
+  saveWorkspace: (workspace) => ipcRenderer.invoke('dexpad:save-workspace', workspace),
   openWorkspace: () => ipcRenderer.invoke('dexpad:open-workspace'),
   refreshWallpaper: () => ipcRenderer.invoke('dexpad:refresh-wallpaper'),
   openUrl: (url) => ipcRenderer.invoke('dexpad:open-url', url),
   onStateUpdated: (callback) => {
+    if (typeof callback !== 'function') return () => {};
     const listener = (_event, state) => callback(state);
     ipcRenderer.on('dexpad:state-updated', listener);
     return () => ipcRenderer.removeListener('dexpad:state-updated', listener);
